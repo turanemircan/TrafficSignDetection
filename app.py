17655b9308e8aa0a36d3b66b1428df0c18e5e0af
batuhan bozkurt <batuhanbozkurt37@hotmail.com>
2025-05-02 23:55:39 +0300

    Çıktı Görsel eklendi iyileştirmeler yapıldı

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from ultralytics import YOLO
 from PIL import Image
 import torch
 import os
-import uuid
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['RESULT_FOLDER'] = 'static/signs'
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = YOLO("C:/Users/Emircan/Documents/Python/Yol_Isareti_Tanima_Sistemi/runs/detect/train/weights/best.pt")
+model = YOLO("C:/Users/Batuhan/Desktop/TrafficSignDetection/runs/detect/train/weights/best.pt")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,18 +34,29 @@ def index():
             predicted_class = "Tespit edilemedi"
             confidence = 0
 
+        example_sign_path = None
+        if predicted_class != "Tespit edilemedi":
+            folder_path = app.config['RESULT_FOLDER']
+            for f in os.listdir(folder_path):
+                if os.path.isfile(os.path.join(folder_path, f)):
+                    name_without_ext = os.path.splitext(f)[0]
+                    if predicted_class == name_without_ext:
+                        example_sign_path = f"signs/{f}" 
+                        break
+
         # Inference süresi
         inference_time = result.speed['inference']
 
-        # Görüntüyü kaydet ve yolu oluştur
-        save_path = os.path.join('static/uploads', img_file.filename)
+        # Yüklenen resmi kaydet
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], img_file.filename)
         img.save(save_path)
 
         return render_template('index.html',
                                image_path=save_path,
                                predicted_class=predicted_class,
                                confidence=confidence,
-                               inference_time=inference_time)
+                               inference_time=inference_time,
+                               example_sign_path=example_sign_path)
     return render_template('index.html')
 
 if __name__ == '__main__':
